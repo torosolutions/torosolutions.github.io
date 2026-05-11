f0eb6fc3502783078e9864a0c0b0e045a5a73599
Duc Nguyen <duc.Nguyen@rfhl.com>
2026-05-11 09:07:35 +0700

    Add company address to contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -25,6 +25,15 @@ const Contact: React.FC = () => {
                   (+84) 0775 325 489
                 </a>
               </div>
+              <div className="flex items-center justify-center gap-3 text-blue-50 mt-4 text-lg">
+                <svg className="w-6 h-6 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+                <address className="not-italic">
+                  Verosa Park, Số 39 Đường số 10, Phường Phú Hữu, TP. Hồ Chí Minh, Việt Nam
+                </address>
+              </div>
             </div>
           </div>
         </div>
